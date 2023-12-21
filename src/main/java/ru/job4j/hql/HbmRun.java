7f90e8cf3a1b121c099094749e3d70ead16e52da
Lyubov <n.lubovj@gmail.com>
2023-12-21 23:59:25 +0300

    Add gitignore. Add Candidate and HbmRun classes

---
+++ src/main/java/ru/job4j/hql/HbmRun.java
@@ -0,0 +1,10 @@
+package ru.job4j.hql;
+
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HbmRun {
+    public static void main(String[] args) {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+    }
+}
